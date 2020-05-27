9df6cb6c34242b3ddce53e6ff506336a4df63333
Saadat-MBP16 <me@saadat.dev>
2020-05-27 19:13:28 +0200

    Update Flask app - Support for JWT based auth - using dotenv now
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-import os
+from os import path, environ as env
 from dotenv import load_dotenv
 load_dotenv()
 
-from Database.MongoDBHandlers import TokenDBHandler
-# MongoTokens = TokenDBHandler()
-
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, "")
+app.config['UPLOAD_FOLDER'] = path.join(app.root_path, "")
+app.config['JWT_SECRET_KEY'] = env.get('JWT_SECRET')
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
 
+JWTManager = JWTManager(app)
 
-@app.route('/')
-def index():
-    return 'Hello World !'
+from Endpoints.Authorization import *
+from Endpoints.LookupAPI import *
 
 
 if __name__ == '__main__':
