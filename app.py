d44054c7dbde5dc8842632eb71dee36d6b9fada4
Saadat-MBP16 <me@saadat.dev>
2020-08-14 00:05:34 +0200

    rebase to "lighter" app-factory pattern
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-from os import path, environ as env
-from dotenv import load_dotenv
-load_dotenv()
-
 from flask import Flask
-from flask_jwt_extended import JWTManager
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = path.join(app.root_path, "")
-app.config['JWT_SECRET_KEY'] = env.get('JWT_SECRET')
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
-
-JWTManager = JWTManager(app)
-
-from Endpoints.Authorization import *
-from Endpoints.LookupAPI import *
 
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)    
+    return app
